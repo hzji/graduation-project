b0e7a19e83754ce8537ab69f2742d510298fce77
hzji <33576786+hzji@users.noreply.github.com>
2018-03-23 16:53:41 +0800

    Update SpatialSpectrum_CACIS.m
    update

--- SpatialSpectrum_CACIS.m
+++ SpatialSpectrum_CACIS.m
@@ -12,47 +12,38 @@ clc;clear all;close all;
 c = 340;
 f = 3400;
 lambda = c/f;
-d = lambda/2;                          % 阵元间距离，取为入射波长的一半
+d = lambda/2;                          % unit spacing = half-wavelength
 T = 2000;                              % snapshots
-N_Sig = 30;                            % 入射信号数量
+N_Sig = 16;                            % the num of input signals
 M = 6;                                 % number of sensor for subarray A 
 N = 7;                                 % number of sensor for subarray B
-DOA = linspace(-60, 60, N_Sig);        % 入射信号角度
-p = 6;                                 % compressed factor p = 2,3,6
+DOA = linspace(-60, 60, N_Sig);        % direction of input signals
+p = 3;                                 % compressed factor p = 2,3,6
 SNR = 15;                              % 信噪比
 
-Fc=[2*10^3:2*10^3/(N_Sig-1):5*10^3];                 % 入射信号频率
+Fc=[2*10^3:2*10^3/(N_Sig-1):5*10^3];   % frequencies of input sig
 
-thetatest=(-90*pi/180:1*pi/180:90*pi/180);               % theta角度搜索范围  
-thetanum=length(thetatest);
-
-
-%% ------------------------信号生成--------------------------------
+%% ----------------------Generate signals-------------------------
 T_Vector=(1:T)/f;  
 p_N = [0:M/p:M*(N-1)/p];   
 p_M = [0:N:(M-1)*N];
-P = union(p_N,p_M)                                     % 物理阵元位置
+P = union(p_N,p_M)                    % the positions of physical sensors
 
-A = zeros(length(P),N_Sig);          
-SignalVector = zeros(N_Sig,T);
+A = zeros(length(P),N_Sig);           % manifold    
+SigVec = zeros(N_Sig,T);
 
-%% 构造非均匀线阵信号
-for Q=1:N_Sig        
-        A(:,Q)=exp(-j*P'*2*pi*d*sin(DOA(Q)*pi/180)/lambda); 
-        SignalVector(Q,:)=exp(1j*2*pi*Fc(Q).*T_Vector);  
+%% Generate the imping Sources
+for Q = 1:N_Sig        
+        A(:,Q) = exp(-j*P'*2*pi*d*sin(DOA(Q)*pi/180)/lambda); 
+        SigVec(Q,:) = exp(1j*2*pi*Fc(Q).*T_Vector);  
 end
-X0=A*SignalVector;
-X=awgn(X0,SNR,'measured');                              % 信号加噪
+x0 = A*SigVec;            
+x = awgn(x0,SNR,'measured');      % Generate noises on Signals
 
-%% ---------------------S_MUSIC Algorithm-------------------------
+%% construct covariance matrix
                                                                        
-R=X*X'/T;                                                            
-z=R(:);                                         % 矢量化 matrix--->vector
-
-%% 提取连续虚拟阵列对应的z1   
-% 虚拟阵列在-M*N+M*(N-1)/factor+1,M*N-M*(N-1)/factor-1上连续
-% 抽出相应的行向量，先确定向量号
-%flag = 1;           %不同互质结构的标识符,1表示CACIS结构,0表示CADiS
+R = x*x'/T;                                                            
+z = R(:);       % vectorization: matrix--->vector
 row_num = extract_row1(P,M,N,p);
 z1 = z(row_num);
 
@@ -62,10 +53,10 @@ for i = 1:MM
     zi = z1(i:i+MM-1);
     Ri(:,:,i) = zi*zi';
 end
-Rzz = sum(Ri,3)/MM;
+Rz = sum(Ri,3)/MM;
 
 %% ---------------------MUSIC algorithm---------------------------
-[U,S,V] = svd(Rzz);
+[U,S,V] = svd(Rz);
 lamda = diag(S);
 ratio = lamda(1:end-1)./lamda(2:end);
 if MM >= N_Sig+1
