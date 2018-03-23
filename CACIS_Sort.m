e0905592dfa3c65c916e26b94c8a69b95e7b0a6f
hzji <33576786+hzji@users.noreply.github.com>
2018-03-23 17:02:57 +0800

    Create CACIS_Sort.m

---
+++ CACIS_Sort.m
@@ -0,0 +1,15 @@
+function z = CACIS_Sort(Z,positions,m,n,p)
+s = 1;
+row_num = zeros(2*m*n-2*m*(n-1)/p-1,1);
+row_matrix = zeros(m+n-1,m+n-1);
+for i = 1:m+n-1
+    row_matrix(i,:) = positions(i) - positions;
+end
+for j = -m*n+m*(n-1)/p+1:m*n-m*(n-1)/p-1
+    a = find(row_matrix == j);
+    row_num(s) = a(1);
+    s = s+1;
+end
+    row_num = row_num';
+    z = Z(row_num);
+end
