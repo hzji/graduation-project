6d1bc6c1141194413e208d84c794cf31d27c3fa9
hzji <33576786+hzji@users.noreply.github.com>
2018-03-24 12:04:21 +0800

    Update SpatialSpectrum_CACIS.m

--- SpatialSpectrum_CACIS.m
+++ SpatialSpectrum_CACIS.m
@@ -11,15 +11,14 @@ clc;clear all;close all;
 %% ----------------------initialization---------------------------
 c = 340;
 f = 3400;
-lambda = c/f;
-d = lambda/2;                          % unit spacing = half-wavelength
+d = c/f/2;                          % unit spacing = half-wavelength
 T = 2000;                              % snapshots
-N_Sig = 16;                            % the num of input signals
+N_Sig = 33;                            % the num of input signals
 M = 6;                                 % number of sensor for subarray A 
 N = 7;                                 % number of sensor for subarray B
 DOA = linspace(-60, 60, N_Sig);        % direction of input signals
-p = 3;                                 % compressed factor p = 2,3,6
-SNR = 15;                              % 信噪比
+p = 6;                                 % compresssion factor p = 2,3,6
+SNR = 10;                               % 信噪比
 
 Fc=[2*10^3:2*10^3/(N_Sig-1):5*10^3];   % frequencies of input sig
 
@@ -34,11 +33,11 @@ SigVec = zeros(N_Sig,T);
 
 %% Generate the imping Sources
 for Q = 1:N_Sig        
-        A(:,Q) = exp(-j*P'*2*pi*d*sin(DOA(Q)*pi/180)/lambda); 
+        A(:,Q) = exp(-j*P'*2*pi*d*sin(DOA(Q)*pi/180)*f/c); 
         SigVec(Q,:) = exp(1j*2*pi*Fc(Q).*T_Vector);  
 end
 x0 = A*SigVec; 
-% Generate noises on Signals
+% Generate noises  on Signals
 x = awgn(x0,SNR,'measured');          % Received data
 
 %% construct covariance matrix                                                                  
@@ -69,7 +68,7 @@ Un = U(:,ix:end);             % noise subspace
 % Spatial spectrum
 N_scan = 1801;
 az_scan = linspace(-pi/2, pi/2, N_scan);
-manifold_scan = exp( -2j*pi/lambda*d* [0:MM-1].'*sin(az_scan) );
+manifold_scan = exp( -2j*pi*f/c*d* [0:MM-1].'*sin(az_scan) );
 BeamPattern = 1./diag( manifold_scan'*(Un*Un')*manifold_scan );
 BeamPattern_norm = abs(BeamPattern)/max(abs(BeamPattern));
 BeamPattern_db = 10*log10(BeamPattern_norm);
